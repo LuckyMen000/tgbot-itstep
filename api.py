e50a352e161d06e271386a18d7f350e638c80c6b
DanilKossilov <kossilovdanil@outlook.com>
2025-03-01 11:49:02 +0500

    3

--- api.py
+++ api.py
@@ -1,1 +1,6 @@
+# Токен бота 
 TOKEN = "8007849055:AAEM2vYTIKSxkZy2yYnbodR87WN7TaAhWRA"
+
+# Апишка погоды 
+API_KEY = "41dc09da6aae7e0f82a4665d15f8a901"
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
